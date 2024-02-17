e54af38f8d161978a10924ea3b23d9246dc63b0e
EasyCodeCoach <ngugimuchene002@gmail>
2024-02-17 13:30:38 +0300

    icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,7 +19,7 @@ export function EditModal({ data }) {
           <div class="relative bg-white rounded-lg shadow ">
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <h3 class="text-xl font-semibold text-gray-900 ">
-                Terms of Service
+                update assignment status
               </h3>
               <button
                 type="button"
