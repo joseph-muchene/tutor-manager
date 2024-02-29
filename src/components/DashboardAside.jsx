59c672c610c4e39e9baa04f40271ffce0900d11a
EasyCodeCoach <ngugimuchene002@gmail>
2024-02-29 23:49:15 +0300

    feat: nav toggle fix

--- src/components/DashboardAside.jsx
+++ src/components/DashboardAside.jsx
@@ -96,7 +96,7 @@ export const DashboardAside = () => {
       </button>
 
       <aside
-        id="default-sidebar"
+        id="sidebar-multi-level-sidebar"
         class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
